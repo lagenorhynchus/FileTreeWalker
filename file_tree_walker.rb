a927a529488bb3e3ec22bd8e7279e47cb92b2365
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-01-19 23:10:19 +0900

    module_functionの利用方法を変更

--- file_tree_walker.rb
+++ file_tree_walker.rb
@@ -1,8 +1,6 @@
 require "find"
 
 module FileTreeWalker
-  module_function
-
   def walk_file_tree(f)
     Find.find(f) do |file|
       if FileTest.file?(file)
@@ -10,6 +8,8 @@ module FileTreeWalker
       end
     end
   end
+
+  module_function :walk_file_tree
 end
 
 if __FILE__ == $0

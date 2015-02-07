7e2a86e02837b6841db39e8b3bfb269703616b15
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-02-08 00:55:01 +0900

    インデントスタイルを変更

--- FileTreeWalker.hs
+++ FileTreeWalker.hs
@@ -4,15 +4,13 @@ import System.Directory (doesDirectoryExist, getDirectoryContents)
 import System.FilePath ((</>))
 
 walkFileTree :: FilePath -> (FilePath -> IO ()) -> IO ()
-walkFileTree f proc =
-  do
-    isDirectory <- doesDirectoryExist f
-    if isDirectory
-      then
-        do
-          files <- getDirectoryContents f
-          mapM_ ((`walkFileTree` proc) . (f </>)) $ filter (`notElem` [".", ".."]) files
-      else proc f
+walkFileTree f proc = do
+  isDirectory <- doesDirectoryExist f
+  if isDirectory
+    then do
+      files <- getDirectoryContents f
+      mapM_ ((`walkFileTree` proc) . (f </>)) $ filter (`notElem` [".", ".."]) files
+    else proc f
 
 main :: IO ()
 main = walkFileTree "C:\\" print

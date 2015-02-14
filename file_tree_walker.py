a3ac79227370fbefcfeac5c41c01eddfaef084ce
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-02-15 00:13:28 +0900

    改行を追加

--- file_tree_walker.py
+++ file_tree_walker.py
@@ -1,9 +1,11 @@
 import os
 
+
 def walk_file_tree(f, proc):
     for root, dirs, files in os.walk(f):
         for file in files:
             proc(os.path.join(root, file))
+
 
 if __name__ == "__main__":
     walk_file_tree("C:\\", lambda f: print(os.path.abspath(f)))
